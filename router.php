36f378198fb9e64eaed3b5b2fe49ccba027f4756
TahirUddinAhmed <01tahirahmed@gmail.com>
2024-10-02 21:06:09 +0530

    remove unnecessary codes

--- router.php
+++ router.php
@@ -1,13 +1,5 @@
 <?php
-// Router login
 
-// if(array_key_exists($uri, $routes)) {
-//     require basePath($routes[$uri]);
-// } else {
-//     // require the 404 error page
-//     http_response_code(404);
-//     require basePath($routes['404']);
-// }
 
 class Router {
     protected $routes = [];
--- routes.php
+++ routes.php
@@ -1,10 +1,4 @@
 <?php 
-// return [
-//     '/' => 'controllers/home.php',
-//     '/login' => 'controllers/users/login.php',
-//     '/signup' => 'controllers/users/signup.php',
-//     '404' => 'controllers/errors/404.php'
-// ];
 
 $router->get('/', 'controllers/home.php');
 $router->get('/login', 'controllers/users/login.php');
